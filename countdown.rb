3af12b366cf7a6ab36dae6835c10fbbadbc8a172
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-05 18:46:19 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,10 @@
 def countdown(n)
   countdown = n
   countdown =- 1
+  return "HAPPY NEW YEAR!"
   while countdown > 0
     puts "#{number} SECOND(S)!"
   end
-    return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(n)
